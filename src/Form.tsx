8641ad3d37c151c0d408c2c54cc8a89b040b88f8
Timothy Loo <TimothyLoo07@gmail.com>
2023-05-06 11:00:36 -1000

    update docker-compose.yml file to track dev changes in real-time

---
+++ src/Form.tsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+import './App.css'
+
+export default function Form() {
+
+  return (
+    <form>
+      <input type='text' placeholder='Input surf spot...' />
+      <input type='submit' />
+    </form>
+  )
+}
