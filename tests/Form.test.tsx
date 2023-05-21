ed746614f8cf3252a082f06d41bf235953eb4f53
Timothy Loo <TimothyLoo07@gmail.com>
2023-05-21 12:27:21 -1000

    fixed jest testing

--- tests/Form.test.tsx
+++ tests/Form.test.tsx
@@ -1,9 +1,11 @@
+// @ts-ignore
+import React from 'react';
 import renderer from 'react-test-renderer';
 import Form from '../src/Form';
 
 describe('Test if Form Renders', () => {
   it('renders correctly', () => {
-    const tree = renderer.create(Form());
+    const tree = renderer.create(<Form />);
     expect(tree).not.toBeNull();
   });
 });
