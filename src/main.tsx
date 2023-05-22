216ff479c18e526121ebcb6268bdef39c4b01254
Timothy Loo <TimothyLoo07@gmail.com>
2023-05-21 15:25:20 -1000

    update to be a pwa

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import Form from './Form.tsx'
 import './index.css'
+// @ts-ignore
+import {unregister, register} from '../src/serviceWorkerRegistration.js';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -10,3 +12,14 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <Form />
   </React.StrictMode>,
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+// serviceWorkerRegistration.unregister();
+register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
